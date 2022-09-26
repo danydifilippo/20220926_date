5103a9d77a830c028e067564f7a1e1391137f1bb
danydifilippo <danydifilippo@gmail.com>
2022-09-26 15:58:16 +0200

    initial

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let dateNow = new Date()
+console.log(dateNow)
+
+document.getElementById('screen').innerHTML=dateNow
+
+let giorno = dateNow.getDay()
+console.log(giorno)
+document.getElementById('screenIt').innerHTML=anno
