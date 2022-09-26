861cb7bc604c6880d01992b7dc3fbdf5d34ab0e5
danydifilippo <danydifilippo@gmail.com>
2022-09-26 22:14:29 +0200

    complete

--- js/script.js
+++ js/script.js
@@ -3,13 +3,12 @@ console.log(dateNow)
 
 document.getElementById('screen').innerHTML=dateNow
 
-giorno = [ "domenica", "lunedì", "martedì", "mercoledì", "giovedì", "venerdì", "sabato"];
+let giorno = [ "domenica", "lunedì", "martedì", "mercoledì", "giovedì", "venerdì", "sabato"];
 let data = dateNow.toLocaleDateString('it-IT')
 let ore = dateNow.toLocaleTimeString()
 document.getElementById('screenIt').innerHTML=`${giorno[dateNow.getDay()]} ${data} alle ${ore}`
 
-function btnSpain() {
-    let dateNow = new Date()
-    let hours = dateNow.toLocaleTimeString('it-IT',{timeZone: "Europe/Lisbon"})
-    document.getElementById('westEUtime').innerHTML=`${hours}`
+function btnFlag(timezone_fusorario) {
+    let hours = dateNow.toLocaleTimeString('it-IT',{timeZone: timezone_fusorario})
+    document.getElementById('EUtime').innerHTML=`${hours}`
 }
